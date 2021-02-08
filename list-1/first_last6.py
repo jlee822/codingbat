87423e36cdf0d84e9f06298bf087f81194fe52ba
James Lee <jlee22@babson.edu>
2021-02-08 00:51:17 -0500

    update

--- list-1/first_last6.py
+++ list-1/first_last6.py
@@ -1,5 +1,7 @@
 def first_last6(nums):
     """Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more."""
+    if nums[:1] == 6 or nums[:len(nums)] == 6: 
+        return 
     return nums[0] == 6 or nums[-1] == 6
 
 
---
+++ warmup-1/monkey_trouble.py
@@ -0,0 +1,10 @@
+def monkey_trouble(a_smile, b_smile):
+    '''We have two monkeys, a and b, and the parameters a_smile and b_smile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return True if we are in trouble.''' 
+    if a_smile == False and b_smile == False:
+        return True
+    else: 
+        return a_smile == True and b_smile == True
+
+print(monkey_trouble(True, True))
+print(monkey_trouble(False, False))
+print(monkey_trouble(True, False))
